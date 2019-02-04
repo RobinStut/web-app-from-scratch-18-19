b7f75950f2c6dcaf6045c50d890c3947012587ad
RobinStut <robin.stut@hva.nl>
2019-02-04 14:21:28 +0100

    simplyfied the callback and added the text printing

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -3,12 +3,12 @@ var urlAPI = 'https://geek-jokes.sameerkumar.website/api'
 console.log("Welcome");
 
 // met behulp van bron: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
-
 var requestAPI = new XMLHttpRequest();
 requestAPI.open("GET", urlAPI, true);
 requestAPI.onload = function (e) {
     if (requestAPI.status === 200) {
-      console.log(requestAPI.responseText);
+      console.log("Juiste status + tekst uitprinten");
+      document.getElementById('joke').innerHTML = requestAPI.responseText;
     } else {
       console.error(requestAPI.statusText);
     }
