766b3b74fe68378b0775a4ab6cb2d4e8d7bd3ea3
RobinStut <robin.stut@hva.nl>
2019-02-06 12:25:18 +0100

    Loaded a new API

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,20 +1,49 @@
-var urlAPI = 'https://geek-jokes.sameerkumar.website/api'
+//var urlAPI = 'https://geek-jokes.sameerkumar.website/api'
+//
+//// met behulp van bron: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
+//var requestAPI = new XMLHttpRequest();
+//requestAPI.open("GET", urlAPI, true);
+//requestAPI.onload = function(e) {
+//  if (requestAPI.status === 200) {
+//    console.log("Juiste status + tekst uitprinten");
+//    document.getElementById('joke').innerHTML = requestAPI.responseText;
+//  } else {
+//    console.error(requestAPI.statusText);
+//  }
+//};
+//
+//requestAPI.onerror = function(e) {
+//  console.error(requestAPI.statusText);
+//};
+//requestAPI.send(null);
+//
+//document.getElementById('button').addEventListener('click', randomJoke);
+//
+//function randomJoke() {
+//  document.getElementById('joke').innerHTML = requestAPI.responseText;
+//}
 
-console.log("Welcome");
+//Old version
+
+
+
+var urlAPI = 'https://pokeapi.co/api/v2/pokemon'
 
 // met behulp van bron: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
 var requestAPI = new XMLHttpRequest();
 requestAPI.open("GET", urlAPI, true);
-requestAPI.onload = function (e) {
-    if (requestAPI.status === 200) {
-      console.log("Juiste status + tekst uitprinten");
-      document.getElementById('joke').innerHTML = requestAPI.responseText;
-    } else {
-      console.error(requestAPI.statusText);
-    }
+requestAPI.onload = function(e) {
+  if (requestAPI.status === 200) {
+    console.log("Juiste status + tekst uitprinten");
+    var reqResult = JSON.parse(requestAPI.responseText)
+    console.log(reqResult.results);
+    // document.getElementById('joke').innerHTML = requestAPI.responseText;
+  } else {
+    console.error(requestAPI.statusText);
+  }
 };
 
-requestAPI.onerror = function (e) {
+requestAPI.onerror = function(e) {
   console.error(requestAPI.statusText);
 };
 requestAPI.send(null);
