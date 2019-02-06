760f61735543b707e7990159457fc6f2f150fed2
RobinStut <robin.stut@hva.nl>
2019-02-06 13:35:14 +0100

    Added a method to display text in HTML

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,49 +1,58 @@
-//var urlAPI = 'https://geek-jokes.sameerkumar.website/api'
-//
-//// met behulp van bron: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
-//var requestAPI = new XMLHttpRequest();
-//requestAPI.open("GET", urlAPI, true);
-//requestAPI.onload = function(e) {
-//  if (requestAPI.status === 200) {
-//    console.log("Juiste status + tekst uitprinten");
-//    document.getElementById('joke').innerHTML = requestAPI.responseText;
-//  } else {
-//    console.error(requestAPI.statusText);
-//  }
-//};
-//
-//requestAPI.onerror = function(e) {
-//  console.error(requestAPI.statusText);
-//};
-//requestAPI.send(null);
-//
-//document.getElementById('button').addEventListener('click', randomJoke);
-//
-//function randomJoke() {
-//  document.getElementById('joke').innerHTML = requestAPI.responseText;
-//}
-
-//Old version
-
-
-
-var urlAPI = 'https://pokeapi.co/api/v2/pokemon'
+var link = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=151'
 
 // met behulp van bron: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
 var requestAPI = new XMLHttpRequest();
-requestAPI.open("GET", urlAPI, true);
+requestAPI.open("GET", link, true);
 requestAPI.onload = function(e) {
   if (requestAPI.status === 200) {
     console.log("Juiste status + tekst uitprinten");
-    var reqResult = JSON.parse(requestAPI.responseText)
-    console.log(reqResult.results);
+    var pokeAPI = JSON.parse(requestAPI.responseText)
+    // Loopt door object heen
+    pokeAPI.results.forEach(function(pokemon) {
+      console.log(pokemon.name + " " + pokemon.url);
+
+      element = document.getElementById("jan");
+      element.innerHTML +=
+
+      `
+      <div class="pokemoncard">
+      <p>${pokemon.name}</p>
+      <p>${pokemon.url}</p>
+      </div>
+      `
+
+      // element.innerHTML = `${pokemon.map((item, i) =>{
+      //   `<div class="incident">
+      //         <p>Casenumber:${pokemon.name}</p>
+      //   </div>`
+      // })}`
+
+//       element.innerHTML = `${pokemon
+// .map((item, i) =>
+//              `
+//        <div class="incident">
+//        <p>Casenumber:${pokemon.name}</p>
+//
+//        </div>
+//      `.trim()
+//            )
+//            .join("")}
+//    `;
+
+
+
+
+
+    })
     // document.getElementById('joke').innerHTML = requestAPI.responseText;
   } else {
     console.error(requestAPI.statusText);
   }
 };
-
 requestAPI.onerror = function(e) {
   console.error(requestAPI.statusText);
 };
 requestAPI.send(null);
+
+
+// document.getElementById('name').innerHTML = "test";
