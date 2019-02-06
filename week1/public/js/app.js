aaf50dffcfb890fc106384aa54df41eb3f22ed5d
RobinStut <robin.stut@hva.nl>
2019-02-06 15:48:45 +0100

    Added a way to pick up images with a dynamic link

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,50 +1,48 @@
-var link = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=151'
+var link = 'https://pokeapi.co/api/v2/pokemon'
+var link2 = 'https://pokeapi.co/api/v2/pokemon'
 
+// var i = 0
 // met behulp van bron: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
 var requestAPI = new XMLHttpRequest();
+var requestPokemon = new XMLHttpRequest();
 requestAPI.open("GET", link, true);
-requestAPI.onload = function(e) {
+requestAPI.onload = async function(e) {
   if (requestAPI.status === 200) {
     console.log("Juiste status + tekst uitprinten");
     var pokeAPI = JSON.parse(requestAPI.responseText)
+    //console.log(pokeAPI)
+    var pokemonArray = []
+    // console.log(pokeAPI)
     // Loopt door object heen
-    pokeAPI.results.forEach(function(pokemon) {
-      console.log(pokemon.name + " " + pokemon.url);
+    for (let i = 0; i < pokeAPI.results.length; i++) {
+      // console.log(i)
+    // pokeAPI.results.forEach(function(pokemon, index) {
+      // console.log(index)
+      // var linkNew = ("https://pokeapi.co/api/v2/pokemon/" + i
 
-      element = document.getElementById("jan");
+      let test = await getData(i);
+
+      console.log(test)
+
+
+      // console.log('https://pokeapi.co/api/v2/pokemon/' + i)
+      // console.log(pokemonArray)
+
+      // console.log(pokeAPI.results[i].name + " " + pokeAPI.results[i].url);
+      element = document.getElementById("wrapper");
       element.innerHTML +=
-
       `
-      <div class="pokemoncard">
-      <p>${pokemon.name}</p>
-      <p>${pokemon.url}</p>
+      <div class="pokemoncard" data-index=${i}>
+      <p>${pokeAPI.results[i].name}
+      https://pokeapi.co/api/v2/pokemon/${pokeAPI.results[i].name}/</p>
+      <a href="${pokeAPI.results[i].url}">${pokeAPI.results[i].name}</p>
+      <img url"https://pokeapi.co/api/v2/pokemon/' + i}">
       </div>
       `
-
-      // element.innerHTML = `${pokemon.map((item, i) =>{
-      //   `<div class="incident">
-      //         <p>Casenumber:${pokemon.name}</p>
-      //   </div>`
-      // })}`
-
-//       element.innerHTML = `${pokemon
-// .map((item, i) =>
-//              `
-//        <div class="incident">
-//        <p>Casenumber:${pokemon.name}</p>
-//
-//        </div>
-//      `.trim()
-//            )
-//            .join("")}
-//    `;
+    }
 
 
 
-
-
-    })
-    // document.getElementById('joke').innerHTML = requestAPI.responseText;
   } else {
     console.error(requestAPI.statusText);
   }
@@ -54,5 +52,15 @@ requestAPI.onerror = function(e) {
 };
 requestAPI.send(null);
 
-
-// document.getElementById('name').innerHTML = "test";
+function getData(i) {
+  return new Promise((resolve, reject) => {
+    requestPokemon.open("GET", `https://pokeapi.co/api/v2/pokemon/${i+1}`, true)
+    requestPokemon.onload = function (e) {
+      // console.log(requestPokemon)
+      resolve(JSON.parse(requestPokemon.responseText))
+      //console.log(JSON.parse(requestPokemon.responseText))
+    //   pokemonArray.push(JSON.parse(requestPokemon.responseText))
+    }
+    requestPokemon.send()
+  })
+}
