f6635ea9bce9b469024387c43b09f0c543cdfa34
RobinStut <robin.stut@hva.nl>
2019-02-11 16:33:56 +0100

    Trying to fix image links

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,59 +1,62 @@
 var link = 'https://pokeapi.co/api/v2/pokemon'
-var link2 = 'https://pokeapi.co/api/v2/pokemon'
-
-// var i = 0
-// met behulp van bron: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
 var requestAPI = new XMLHttpRequest();
 var requestPokemon = new XMLHttpRequest();
-requestAPI.open("GET", link, true);
-requestAPI.onload = async function(e) {
-  if (requestAPI.status === 200) {
-    console.log("Juiste status + tekst uitprinten");
-    var pokeAPI = JSON.parse(requestAPI.responseText)
-    //console.log(pokeAPI)
-    var pokemonArray = []
+var pokeAPI = '';
+var pokeAPI2 = '';
 
-    // console.log(pokeAPI)
-    // Loopt door object heen
-    for (let i = 0; i < pokeAPI.results.length; i++) {
-  
-      let pokemonDetails = await getData(i);
+// met behulp van bron: https://codepen.io/joostf/pen/OQxpxx?editors=1010
 
-      console.log(pokemonDetails)
-      console.log(pokemonDetails.sprites.front_default)
+var loadData = new Promise(function(resolve, reject){
+    var request = new XMLHttpRequest();
+    request.open('GET', link, true)
 
-      element = document.getElementById("wrapper");
-      element.innerHTML +=
-
-      `
-      <div class="pokemoncard" data-index=${i}>
-      <img url"${pokemonDetails.sprites.front_default}">
-      <p>${pokeAPI.results[i].name}
-      https://pokeapi.co/api/v2/pokemon/${pokeAPI.results[i].name}/</p>
-      <a href="${pokeAPI.results[i].url}">${pokeAPI.results[i].name}</a>
-      </div>
-      `
-    }
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+       // Success!
+        pokeAPI = JSON.parse(request.responseText);
+        resolve(pokeAPI);
+        console.log('gelukt');
 
 
+        //     // Start of loop
+        pokeAPI.results.forEach(function(item) {
+          // multipleLinks()
+          var element = document.getElementById("wrapper");
+          //console.log(item.name);
+          //console.log(item.url);
+          element.innerHTML +=
+          `
+          <div class="pokemoncard" data-index=${item}>
+          <p>${item.name}
+          https://pokeapi.co/api/v2/pokemon/${item.name}/</p>
+          <a href="${item.url}">${item.name}</a>
+          </div>
+          `
+        })
 
-  } else {
-    console.error(requestAPI.statusText);
-  }
-};
-requestAPI.onerror = function(e) {
-  console.error(requestAPI.statusText);
-};
-requestAPI.send(null);
+        pokeAPI.results.forEach(function(item2){
+           console.log(item2);
+         var loadData2 = new Promise((resolve, reject) => {
+             requestPokemon.open("GET", link.item2, true)
+             requestPokemon.onload = function(e) {
+               pokeAPI2 = JSON.parse(requestPokemon.responseText);
+               resolve(pokeAPI2);
+               console.log(item2)
+               }
+             requestPokemon.send()
+         })
+         }
+         )
 
-function getData(i) {
-  return new Promise((resolve, reject) => {
-    requestPokemon.open("GET", `https://pokeapi.co/api/v2/pokemon/${i+1}`, true)
-    requestPokemon.onload = function (e) {
-      // console.log(requestPokemon)
-      resolve(JSON.parse(requestPokemon.responseText))
+      } else {
+       // We reached our target server, but it returned an error
+        reject(error);
+      }
+    };
 
-    }
-    requestPokemon.send()
-  })
-}
+    request.onerror = () => {
+      // There was a connection error of some sort
+    };
+
+    request.send();
+  });
