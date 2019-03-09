6e430924533559e9e2d8aaa948099eb4ae89915f
RobinStut <robin.stut@hva.nl>
2019-03-09 18:57:03 +0100

    fixed api request

--- herkansing/public/js/app.js
+++ herkansing/public/js/app.js
@@ -3,11 +3,55 @@ console.log('welkom');
 var app = {
   init: function() {
     console.log('init');
+    api.get();
   },
-  development:function(){
-console.log('developer');
+  development: function() {
+    console.log('developer');
   }
 }
+
+var api = {
+  contain: {
+    details: []
+  },
+
+  get: async function() {
+    var url = 'https://pokeapi.co/api/v2/pokemon/?limit=30&offset=0'
+    var list = await this.call(url);
+    list.results.map(function(item) {
+      api.detail(item.url)
+    })
+    data.filter(this.contain.details)
+  },
+
+  detail: async function(url) {
+    var list = [await api.call(url)];
+    Array.prototype.push.apply(api.contain.details, list);
+  },
+
+  call: async function(url) {
+    var data = await fetch(url);
+    var res = await data.json();
+    return res
+  },
+}
+
+var data = {
+  filter: function(receivedData) {
+    var data = receivedData;
+    console.log(typeof data);
+    console.log(Array.isArray(data));
+    console.log(data);
+    console.log([data][1]);
+    console.log(data.base_experience);
+  },
+}
+
+
+
+
+
+
 
 
 app.init();
